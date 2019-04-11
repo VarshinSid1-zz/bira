b183aae9b2030b7aa60149a51f4ecc7f47faf005
varshin <varshin.s@hashedin.com>
2019-04-11 16:03:47 +0530

    Code optimised

--- biraapp/bira/urls.py
+++ biraapp/bira/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import get_project_by_id, projects_list, projects, create_issue, get_issues_by_id, assign_user,\
-        get_issues_by_user_id
+from .views import get_project_details, get_all_projects, projects, create_issue, get_all_issues_of_project, \
+    assign_user, get_issues_by_user_id, update_issue_status
 from django.urls import path
 
 urlpatterns = {
-    path("projects/", projects_list, name="projects_list"),
-    path("projectlists/", projects, name="projects"),
-    path("project/id/<str:project_id>/", get_project_by_id, name="get_project_by_id"),
+    path("projects/", get_all_projects, name="get_all_projects"),
+    path("project/id/<str:project_id>/", get_project_details, name="get_project_details"),
     path("createissue/", create_issue, name="create_issue"),
-    path("issue/id/<str:project_assigned>/", get_issues_by_id, name="get_issues_by_id"),
+    path("issue/id/<str:project_assigned>/", get_all_issues_of_project, name="get_all_issues_of_project"),
+    path("update_issue/", assign_user, name="assign_user"),
     path("issue/userid/<str:user_id>/", get_issues_by_user_id, name="get_issues_by_user_id"),
-    path("issue/id/<str:issue_id>/<str:user_id>/", assign_user, name="assign_user"),
+    path("update_issue_status/", update_issue_status, name="update_issue_status"),
+    path("projectlists/", projects, name="projects"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- biraapp/bira/views.py
+++ biraapp/bira/views.py
@@ -1,34 +1,28 @@
 from rest_framework import status
-from .serializers import IssueSerializer
-from .models import Projectlist, Issue
+from .serializers import IssueSerializer, ProjectlistSerializer
+from .models import Projectlist, Issue, User
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
-
-def projects_list(request):
+#1)get_all_projects
+def get_all_projects(request):
     try:
         projects = Projectlist.objects.all()
         data = list(projects.values("project_name","project_id","date_created","date_modified"))
-        return JsonResponse({'data': data},status=200)
+        return JsonResponse(data,status=200,safe=False)
     except Projectlist.DoesNotExist:
         return JsonResponse('Projects exist', status=404)
 
-
-def get_project_by_id(request, project_id):
+#2)Get project details by id: get_project_details( project_id)
+def get_project_details(request, project_id):
     try:
-        project = Projectlist.objects.filter(pk=project_id)
-        data = list(project.values("project_name"))
-        return JsonResponse({'data': data},status=200)
+        project = Projectlist.objects.get(pk=project_id)
+        serializer = ProjectlistSerializer(project)
+        return JsonResponse(serializer.data,status=200)
     except Projectlist.DoesNotExist:
         return JsonResponse('This project_id does not exist', status=404)
 
-
-def projects(request):
-    project_list = Projectlist.objects.order_by('-date_created')[:50]
-    output = ', '.join([p.project_name for p in project_list])
-    return JsonResponse({'data': output})
-
-
+#3)create_issue(title, labels, assignee, type, sprint, status)
 @api_view(['GET','POST'])
 def create_issue(request):
     if request.method == 'GET':
@@ -42,15 +36,28 @@ def create_issue(request):
             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-def get_issues_by_id(request, project_assigned):
+#4)get_all_issues_of_project(project_id)
+def get_all_issues_of_project(request, project_assigned):
     try:
         issue = Issue.objects.filter(project_assigned=project_assigned)
         data = list(issue.values("issue_title", "type", "sprint", "status"))
-        return JsonResponse({'data': data},status=200)
+        return JsonResponse(data,status=200,safe=False)
     except Projectlist.DoesNotExist:
         return JsonResponse('This project_id does not exist', status=404)
 
+#6)Assign an issue to a user: assign_issue(issue_id, user_id)
+def assign_user(request):
+    try:
+        issue = Issue.objects.get(issue_id=request.GET['issue_id'])
+        user = User.objects.get(user_id=request.GET['user_id'])
+        issue.assignee = user
+        issue.save()
+        serializer = IssueSerializer(issue)
+        return JsonResponse(serializer.data,status=200)
+    except Projectlist.DoesNotExist:
+        return JsonResponse('assign user got failed', status=404)
+
+#7)Get all issues assigned to the logged in user: get_issue_assigned_to_user( user_id)
 def get_issues_by_user_id(request, user_id):
     try:
         issue = Issue.objects.filter(assignee=user_id)[:50]
@@ -59,10 +66,19 @@ def get_issues_by_user_id(request, user_id):
     except Projectlist.DoesNotExist:
         return JsonResponse('This project_id does not exist', status=404)
 
-def assign_user(request, issue_id, user_id):
+#8)Get all issues assigned to the logged in user: get_issue_assigned_to_user( user_id)
+def update_issue_status(request):
     try:
-        issue = Issue.objects.filter(project_assigned=issue_id)
-        data = list(issue.values("issue_title", "type", "sprint", "status"))
-        return JsonResponse({'data': data},status=200)
+        issue = Issue.objects.get(issue_id=request.GET['issue_id'])
+        status = request.GET['status']
+        issue.status = status
+        issue.save()
+        serializer = IssueSerializer(issue)
+        return JsonResponse(serializer.data,status=200)
     except Projectlist.DoesNotExist:
-        return JsonResponse('This project_id does not exist', status=404)
+        return JsonResponse('status update got failed', status=404)
+
+def projects(request):
+    project_list = Projectlist.objects.order_by('-date_created')[:50]
+    output = ', '.join([p.project_name for p in project_list])
+    return JsonResponse({'data': output})
